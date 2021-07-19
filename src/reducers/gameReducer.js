a763fe1c5ce926207aaf3c5b321b07649ceaa9ee
Natalia <n.garridoagudo@gmail.com>
2021-07-19 14:56:17 +0200

    limpio codigo comentado

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,13 +1,5 @@
 import { types } from "../types/types";
 
-// const initialState = {
-//   idRespuestaMarcada: null,
-//   idRespuestaCorrecta: null,
-//   respuestasAcertadas: 0,
-//   contador: 0,
-//   startedGame: false,
-// };
-
 const initialState = {
   quizObject: null,
   idCorrectAnswer: null,
@@ -21,18 +13,13 @@ const initialState = {
 export const gameReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.startGame:
-      //debugger;
       return {
         ...state,
         startedGame: true,
-        //Esto debe ir en cada dispatch al cargar la nueva pregunta, una que se llame cargarNuevaPregunta
-        // quizObject: action.payload,
-        // idCorrectAnswer: action.payload.correctAnswer.id,
       };
     case types.loadQuestion:
       return {
         ...state,
-        // startedGame: true,
         quizObject: action.payload,
         idCorrectAnswer: action.payload.correctAnswer.id,
       };
@@ -46,11 +33,6 @@ export const gameReducer = (state = initialState, action) => {
         correctAnswersCount: state.correctAnswersCount + resultAnswerCorrect,
       };
 
-    case types.obtenerResultadosFiltrados:
-      return {
-        ...state,
-        resultadosFiltrados: action.payload,
-      };
     case types.nextQuestion:
       return {
         ...state,
@@ -60,43 +42,6 @@ export const gameReducer = (state = initialState, action) => {
             : (state.counter = 0),
         idUserAnswer: null,
       };
-    // case types.pasarAlSiguiente:
-    //   return {
-    //     ...state,
-    //     contador:
-    //       action.payload.contador < action.payload.numeroTotalPreguntas
-    //         ? state.contador + 1
-    //         : (state.contador = 0),
-    //   };
-
-    case types.comprobarRespuesta:
-      // if (
-      //   action.payload.idRespuestaMarcada === action.payload.idRespuestaCorrecta
-      // ) {
-      //   state.respuestasAcertadas = state.respuestasAcertadas + 1;
-      // }
-      if (
-        action.payload.idRespuestaMarcada === action.payload.idRespuestaCorrecta
-      )
-        state.respuestasAcertadas = state.respuestasAcertadas + 1;
-
-      // const respuestaCorrecta = action.payload.listaPosiblesRespuestas.filter(
-      //   (o) => o.id === action.payload.idImagen
-      // )[0];
-      return {
-        ...state,
-        // idRespuestaCorrecta: respuestaCorrecta.id,
-        idRespuestaMarcada: action.payload.idRespuestaMarcada,
-        idRespuestaCorrecta: action.payload.idRespuestaCorrecta,
-      };
-    case types.resetRespuestas:
-      return {
-        ...state,
-        idRespuestaMarcada: null,
-        idRespuestaCorrecta: null,
-      };
-    case types.resetRespuestasAcertadas:
-      return { ...state, respuestasAcertadas: 0 };
 
     case types.reset:
       return initialState;
