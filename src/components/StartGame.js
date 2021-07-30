94ac71f3d79237fc3aafb14061ca61eae1148be8
Natalia <n.garridoagudo@gmail.com>
2021-07-30 13:59:18 +0200

    cleaning

--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -1,26 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { loadQuestion, startGame } from "../actions/cambiarEstadoRespuesta";
+import { startGame } from "../actions/cambiarEstadoRespuesta";
 import { getCats } from "../helpers/getData";
 
 export const StartGame = () => {
-  // useEffect(() => {
-  //   getCats().then((quizObject) => {
-  //     dispatch(startGame(quizObject));
-  //   });
-  // }, []);
   const dispatch = useDispatch();
   const handleStartGame = () => {
     getCats().then(dispatch(startGame()));
-    // dispatch(startGame()).then(dispatch(loadQuestion(quizObject)));
-    // debugger;
-    // dispatch(loadQuestion(quizObject));
   };
-  // const handleStartGame = () => {
-  //   getCats().then((quizObject) => {
-  //     dispatch(startGame(quizObject));
-  //   });
-  // };
 
   return (
     <div className="startGame-btn">
